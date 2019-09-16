bb2f19cb2f4e545fb3242acd6a92862df0a5747c
David Nankivell <32204274@student.murdoch.edu.au>
2019-09-16 19:41:44 +0800

    Initial Player Controller

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb = null;
+    private float horizontalAxisValue = 0.0f;
+    private float verticalAxisValue = 0.0f;
+    [SerializeField]
+    private float speed = 5.0f;
+    private float xRotate = 0.0f;
+    private float yRotate = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate() {
+
+        Vector3 fForward = transform.forward * verticalAxisValue;
+        Vector3 fRight = transform.right * horizontalAxisValue;
+        
+        // Debug.Log(f);
+        rb.AddForce(fForward * speed);
+        rb.AddForce(fRight * speed);
+
+        // transform.position += transform.forward * verticalAxisValue * speed;
+        // transform.position += transform.right * horizontalAxisValue * speed;
+
+        transform.rotation = Quaternion.Euler(0, yRotate, 0);
+        Camera.main.transform.rotation = Quaternion.Euler(xRotate, yRotate, 0);
+
+    }
+
+    public float HorizontalValue {
+        set { horizontalAxisValue = value; }
+    }
+
+    public float VerticalValue {
+        set { verticalAxisValue = value; }
+    }
+
+    public float RotateX {
+        set { 
+                float x = xRotate;
+                x += -value * 5.0f;
+                xRotate = Mathf.Clamp(x, -89.0f, 89.0f);
+        }
+    }
+
+    public float RotateY {
+        set { yRotate += value * 5.0f; }
     }
 }
--- Game/Assets/Scripts/PlayerInput.cs
+++ Game/Assets/Scripts/PlayerInput.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerController))]
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    private PlayerController player = null;
+
+    void Start() {
+        player = GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        player.HorizontalValue = horizontal;
+
+        player.VerticalValue = vertical;
+
+        if (Input.GetButtonDown("Jump")) {
+            // Debug.Log("Jump");
+        }
+
+        if (mouseX != 0.0f) {
+            player.RotateY = mouseX;
+        }
+
+        if (mouseY != 0.0f) {
+            player.RotateX = mouseY;
+        }
     }
 }
